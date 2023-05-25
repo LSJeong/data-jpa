01e4dd1ae4afa899d2e474c49c2cfeb4901e31ca
LSJeong <lsj930224@naver.com>
2023-05-25 11:38:45 +0900

    Auditing

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import java.util.Optional;
 import java.util.UUID;
 
-@EnableJpaAuditing
+@EnableJpaAuditing  //저장시점에 저장데이터만 입력하고 싶으면 @EnableJpaAuditing(modifyOnCreate = false)
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -17,8 +17,10 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	//등록자, 수정자를 처리해주는 AuditorAware 스프링 빈 등록
 	@Bean
 	public AuditorAware<String> auditorProvider(){
+		//실무에서는 세션 정보나, 스프링 시큐리티 로그인 정보에서 ID를 받음
 		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 }
