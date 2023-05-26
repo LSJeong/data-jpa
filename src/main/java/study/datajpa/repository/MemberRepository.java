93a855927691e6d004af8b3d3870e9a8dd460cc1
LSJeong <lsj930224@naver.com>
2023-05-26 10:52:57 +0900

    네이티브 쿼리

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -81,4 +81,13 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     //List<UsernameOnlyDto> findProjectionsByUsername(@Param("username") String username);
     <T> List<T> findProjectionsByUsername(@Param("username") String username, Class<T> type); //동적 Projections
 
+    //네이티브 쿼리
+    @Query(value = "select * from member where username = ?", nativeQuery = true)
+    Member findByNativeQuery(String username);
+
+    @Query(value = "select m.member_id as id, m.username, t.name as teamName " +
+            "from member m left join team t",
+            countQuery = "select count(*) from member",
+            nativeQuery = true)
+    Page<MemberProjection> findByNativeProjection(Pageable pageable);
 }
